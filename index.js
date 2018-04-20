c2192f535e550942962d5b22f116c42122733696
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-04-20 05:22:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-funcion iterativeLog(array){
+function iterativeLog(array){
   array.forEach( (element, index, array) => {console.log(`${index}:${element}`) })
 }
 
 log = {"Kim", "Than", "Is", "Fat"}
+
+iterativeLog(log)
